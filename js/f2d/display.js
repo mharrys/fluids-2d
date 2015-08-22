57621e7fa45ba5f584f69bb460bf32fc1b1ee341
Mattias Harrysson <mathar@kth.se>
2015-08-22 11:02:33 +0200

    remove quad from scope

--- js/f2d/display.js
+++ js/f2d/display.js
@@ -26,12 +26,11 @@ var F2D = F2D === undefined ? {} : F2D;
             depthTest: false,
             blending: THREE.NoBlending
         });
-
-        this.quad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), this.material);
+        var quad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), this.material);
 
         this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
         this.scene = new THREE.Scene();
-        this.scene.add(this.quad);
+        this.scene.add(quad);
     };
 
     F2D.Display.prototype = {
