78d4c83bb0ad7eef523b62e9425c08dfe11bf866
Mattias Harrysson <mathar@kth.se>
2015-08-22 12:21:45 +0200

    move in splat grid size in shader

--- shaders/splat.fs
+++ shaders/splat.fs
@@ -15,7 +15,7 @@ void main()
 {
     vec2 uv = gl_FragCoord.xy / gridSize.xy;
     vec3 base = texture2D(read, uv).xyz;
-    vec2 fragCoord = point.xy - gl_FragCoord.xy;
-    vec3 splat = color * gauss(fragCoord, radius);
+    vec2 coord = point.xy - gl_FragCoord.xy;
+    vec3 splat = color * gauss(coord, gridSize.x * radius);
     gl_FragColor = vec4(base + splat, 1.0);
 }
